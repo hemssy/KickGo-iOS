d064b975c8ae7dac29e2deede818dc1911a867c1
hemssy <hemsyeons@gmail.com>
2025-10-13 20:49:46 +0900

    [Feat] #3 - 네이버지도 API 가져오기

--- KickGo/KickGo/Controller/MapViewController.swift
+++ KickGo/KickGo/Controller/MapViewController.swift
@@ -1,11 +1,15 @@
 import UIKit
 import SnapKit
+import NMapsMap
 
 class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "지도"
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
     }
 }
 
